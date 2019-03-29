65254b896624ca5fbe9ca8d680eb51204dea9470
unknown <dananasas@gmail.com>
2019-03-29 17:34:11 +0000

    player moving with keys for now

---
+++ MyoGame/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 20;
+
+    void Move(){
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal, 0) * speed;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+}

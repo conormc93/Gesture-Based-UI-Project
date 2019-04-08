9264ae7f85d8fa1f415bf3a60da070aee9dd0a27
unknown <dananasas@gmail.com>
2019-04-08 15:13:19 +0100

    added a splash screen fading in and out

---
+++ MyoGame/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    //Constants and Variables
+    public Image splashImage;
+
+    IEnumerator Start()
+    {
+        //Setting alpha to 0.0f
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        //Fading in and fading out splash screen
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(1);
+    }
+
+    //Method that fades in splash image
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    //Method that fades out splash image
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}

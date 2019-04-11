067cd6155bda5a313f16128c424c1cbdf9ae25bb
unknown <dananasas@gmail.com>
2019-04-11 20:00:59 +0100

    added myo controls to main menu, pose fist for play and pose fingerspead for exit

---
+++ MyoGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject myo = null;
+
+    void Update()
+    {
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+
+            if (thalmicMyo.pose == Pose.Fist)
+            {
+                SceneManager.LoadScene(2);
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+            else if (thalmicMyo.pose == Pose.FingersSpread)
+            {
+                Application.Quit();
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+        
+    }
+
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
+}
